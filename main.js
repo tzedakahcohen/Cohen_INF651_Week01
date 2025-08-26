0132da1bac81c9bb049cc15d785df7372f99653f
tzedakahcohen <92765434+tzedakahcohen@users.noreply.github.com>
2025-08-25 20:52:59 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,41 @@
+const myImage = document.querySelector("img");
 
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello world!";
+myImage.addEventListener("click", () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/dandelon.jpg") {
+    myImage.setAttribute("src", "images/dandelon.jpg");
+  } else {
+    myImage.setAttribute("src", "images/dandelon.jpg");
+  }
+});
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  localStorage.setItem("name", myName);
+  myHeading.textContent = `Mozilla is cool, ${myName}`;
+}
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Mozilla is cool, ${storedName}`;
+}
+
+myButton.addEventListener("click", () => {
+  setUserName();
+});
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
+  }
+}
+
